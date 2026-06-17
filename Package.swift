6ea9d23eb098d0ca148be4cb81464a481018bfa2
Mariano Rezk <marianorezk@gmail.com>
2026-06-17 19:18:05 -0300

    Package: declare watchOS 9 platform
    Without an explicit watchOS platform, SwiftPM derives a default
    deployment target (8.0) below the supported floor on Xcode 26+,
    breaking watchOS builds. Declare .watchOS(.v9), which requires
    swift-tools-version 5.7 (where .v9 was introduced).

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -9,6 +9,7 @@ let package = Package(
         .iOS(.v12),
         .tvOS(.v12),
         .macOS(.v10_13),
+        .watchOS(.v9),
     ],
     products: [
         .library(name: "DateToolsSwift", targets: ["DateToolsSwift"]),
